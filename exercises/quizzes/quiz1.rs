abd55d60df3c2244a04a4efc3f2a7abec58371da
pierandreadelise <pier.delise@teladochealth.com>
2025-03-25 11:52:06 +0100

    4

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -12,8 +12,17 @@
 // the quantity bought.
 // fn calculate_price_of_apples(???) -> ??? { ??? }
 
+fn calculate_price_of_apples(quantity: i32) -> i32 {
+    if quantity <= 40 {
+        quantity * 2
+    } else {
+        quantity * 1
+    }
+}
+
 fn main() {
     // You can optionally experiment here.
+    calculate_price_of_apples(52);
 }
 
 // Don't change the tests!
